81ad07f27f50722645a8306a3c3810ad7b44459d
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-03-22 15:31:43 +0200

    some changes to layout of options fragment and typo fixes

--- PurrfectEvolution/app/src/main/java/purrfect/evolution/OptionsFragment.java
+++ PurrfectEvolution/app/src/main/java/purrfect/evolution/OptionsFragment.java
@@ -43,7 +43,7 @@ public class OptionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_options , container, false);
         TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         textView.setText("another fragmemt");
         return rootView;
