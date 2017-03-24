d9ab34a35c684065ab94ea20f798c076242d5772
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-03-24 11:47:12 +0200

    Added class building which will be base for different building types

---
+++ PurrfectEvolution/app/src/main/java/purrfect/evolution/Building.java
@@ -0,0 +1,57 @@
+package purrfect.evolution;
+
+
+import android.os.Bundle;
+/**
+ * Created by jyhy on 3/24/17.
+ */
+enum BuildingType{
+    SCRATCHINPOST,      //produces happines
+    FEEDING_STATION,    //produces happines
+    CHEW_MOUSE,         //produces happines
+    YARN_BALL,          //creates yarnballs in to the cat screen that can tapped --> gives happines
+    CATNIP,             //produces happines
+
+
+}
+
+
+public abstract class Building {
+
+
+
+    Building(){
+
+    }
+    private float mBaseProductionTime;
+    private float mBaseProductionAmount;
+    private float mBuildingLevel=0;
+    private float mBaseCost;
+
+
+    private float getmBaseCost() {
+        return mBaseCost;
+    }
+
+    public float getmBaseProductionAmount() {
+        return mBaseProductionAmount;
+    }
+
+    public float getmBaseProductionTime() {
+        return mBaseProductionTime;
+    }
+
+    public float getmBuildingLevel() {
+        return mBuildingLevel;
+    }
+
+    public void setmBuildingLevel(float mBuildingLevel) {
+        this.mBuildingLevel = mBuildingLevel;
+    }
+
+    public abstract float getCurrentProductionAmount();
+    public abstract float getCurrentProductionTime();
+    public abstract float getCurrentCost();
+
+
+}
