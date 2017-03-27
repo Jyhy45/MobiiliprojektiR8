b220df619c505a861164e17c13b7b01ddeb53b61
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-03-27 14:21:21 +0300

    added javadoc style comments for datacontainer class

--- PurrfectEvolution/app/src/main/java/purrfect/evolution/DataContainerForPurfectEvolution.java
+++ PurrfectEvolution/app/src/main/java/purrfect/evolution/DataContainerForPurfectEvolution.java
@@ -24,12 +24,23 @@ final class DataContainerForPurfectEvolution {
     {
 
     }
+
+    
+    /**
+     * Increment all click variables
+     * */
     public static void receivedClick(){
         mLifeTimeClicks++;
         mSessionClicks++;
         mThisCycleClicks++;
 
     }
+
+
+    /**
+     * Increments all money variables
+     * @param money Amount of money to be incremented
+     * */
     public static void earnedMoney(double money){
         mLifeTimeMoneyEarnings+=money;
         mSessionMoneyEarnings+=money;
@@ -37,6 +48,12 @@ final class DataContainerForPurfectEvolution {
         mCurrentMoney+=money;
 
     }
+
+
+    /**
+     * Encerements all happines variables
+     * @param happines amount of happpines to added to variables
+     * */
     public static void earnedHappines(double happines){
         mLifeTimeHappinesEarnings+=happines;
         mSessionHappinesEarnings+=happines;
@@ -44,24 +61,45 @@ final class DataContainerForPurfectEvolution {
         mCurrentHappines+=happines;
 
     }
-    public static boolean spenthappines(double spenthappines){
-        if (Double.compare(spenthappines,mCurrentMoney)>=0)
+
+
+    /**
+     * Spends happines if there is enaugh happines to spend
+     * @param mSpentHappines amount of happines to be spent will only spend happines
+     *                       if there is enough happines
+     *
+     * @return Return true if happines was used; return false if happines was not spent
+     * */
+    public static boolean spentHappines(double mSpentHappines){
+        if (Double.compare(mSpentHappines,mCurrentMoney)>=0)
         {
-            mCurrentMoney-=spenthappines;
+            mCurrentMoney-=mSpentHappines;
             return true;
         }else{
             return false;
         }
     }
-    public static boolean spentMoney(double spentmoney){
-        if (Double.compare(spentmoney,mCurrentMoney)>=0)
+
+
+    /**
+     * Spends money if there is enough money to spend
+     * @param mSpentMoney amount of money to be spent will only spend money
+     *                       if there is enough money
+     *
+     * @return Return true if money was used; return false if money was not spent
+     * */
+    public static boolean spentMoney(double mSpentMoney){
+        if (Double.compare(mSpentMoney,mCurrentMoney)>=0)
         {
-            mCurrentMoney-=spentmoney;
+            mCurrentMoney-=mSpentMoney;
             return true;
         }else{
             return false;
         }
     }
+
+
+    /** Resets cycle variables*/
     public static void resetCycle(){
         mThisCycleClicks=0;
         mThisCycleHappinesEarnings=0;
