69d23db010b0c4ae1e009a4d895a5b6cfd709e25
VITUNSUOPASKA\Henri <henri.vesterinen@gmail.com>
2017-04-21 13:13:31 +0300

    options menu doodle

--- PurrfectEvolution/app/src/main/java/purrfect/evolution/OptionsFragment.java
+++ PurrfectEvolution/app/src/main/java/purrfect/evolution/OptionsFragment.java
@@ -13,6 +13,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
@@ -22,6 +25,8 @@ public class OptionsFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+
+    RelativeLayout optionsRelativeLayout;
 
     public OptionsFragment() {
     }
@@ -37,12 +42,70 @@ public class OptionsFragment extends Fragment {
         return fragment;
     }
 
+    public void onClick(View view, Context context)
+    {
+        int state = 0;
+        int state2 = 0;
+
+        switch(view.getId()) {
+            case R.id.On1:
+                if (state == 1) {
+
+                }
+                else if (state == 0) {
+                }
+
+                break;
+            case R.id.On2:
+                if (state2 == 1) {
+                }
+                else if (state2 == 0) {
+                }
+                break;
+            case R.id.Off1:
+                if (state == 1) {
+                }
+                else if (state == 0) {
+                }
+                break;
+            case R.id.Off2:
+                if (state2 == 1) {
+                }
+                else if (state2 == 0) {
+                }
+                break;
+
+
+    }
+
+    }
+
+    public void buttonToggle(View view, Context context)
+    {
+        ImageButton imageButton;
+        imageButton = (ImageButton)view;
+        ImageButton imageButton2;
+
+
+
+
+        imageButton.setImageDrawable(getResources().getDrawable(R.drawable.off_harm));
+
+        imageButton.setImageDrawable(getResources().getDrawable(R.drawable.off_pun));
+
+        imageButton.setImageDrawable(getResources().getDrawable(R.drawable.on_harm));
+
+        imageButton.setImageDrawable(getResources().getDrawable(R.drawable.on_vihr));
+
+    }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_options , container, false);
+        optionsRelativeLayout = (RelativeLayout) rootView;
         return rootView;
     }
 
