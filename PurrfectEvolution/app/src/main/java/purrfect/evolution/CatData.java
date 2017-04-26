0614ddc2cdd10aa51e01c13bf20be0d5892abde9
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-04-26 20:43:07 +0300

    fixed bug

--- PurrfectEvolution/app/src/main/java/purrfect/evolution/CatData.java
+++ PurrfectEvolution/app/src/main/java/purrfect/evolution/CatData.java
@@ -24,7 +24,7 @@ class CatData {
     int mhanta;
 
     public int getMpaa() {
-        if (mhanta!=0){
+        if (mpaa!=0){
             return mpaa;
         }else{
             return mpaa = R.drawable.porrokissa_paa;
